ccaf06c0f69e1e5e3f29f2fc843f2b52a05d88d4
Gokuljayan494 <gokuljayan494@gmail.com>
2022-12-12 14:51:37 +0530

    new

--- controller/adsController.js
+++ controller/adsController.js
@@ -1,6 +1,27 @@
 let AdsModel = require('../model/adsModel');
 const multer = require('multer');
 
+// const cloudinary = require('cloudinary').v2;
+// const CloudinaryStorage = require('multer-storage-cloudinary');
+// // cloudinary.config({
+// //   cloud_name: 'YOUR_CLOUD_NAME',
+// //   api_key: 'YOUR_API_KEY',
+// //   api_secret: 'YOUR_API_SECRET',
+// // });
+
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+
+// const storage1 = CloudinaryStorage({
+//   cloudinary: cloudinary,
+//   params: {
+//     folder: 'DEV',
+//   },
+// });
+///////////////////////
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, './public');
